0dac16a87cb6ae92d92b4fe9150a60a7dea89a30
mugiirar <munenevincentmugiira@gmail.com>
2023-08-14 04:32:45 -0700

    unchanged

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 4-new_in_list.py
+
+def new_in_list(my_list, idx, element):
+    """Replace an element in a copied list at a specific position."""
+
+    new_list = my_list[:]
+
+    if idx < 0 or idx >= len(new_list):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
