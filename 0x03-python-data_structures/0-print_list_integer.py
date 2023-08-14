258c1b49d49f82f535c31106f2a79dce54f67001
mugiirar <munenevincentmugiira@gmail.com>
2023-08-14 00:49:07 -0700

    integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+
+def print_list_integer(my_list=[]):
+
+    i = 0;
+    while i <= (len(my_list) - 1):
+        print("{:d}".format(my_list[i]))
+        i += 1
