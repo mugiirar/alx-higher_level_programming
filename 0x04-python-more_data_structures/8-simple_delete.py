c7f9b0c7b1d49dc52cb3eb7fa5604df6236ca305
mugiirar <munenevincentmugiira@gmail.com>
2023-08-17 08:30:54 -0700

    deldeted

--- 0x04-python-more_data_structures/8-simple_delete.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# 8-simple_delete.py
-
-def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key) is not None:
-        a_dictionary.pop(key)
-    return a_dictionary
