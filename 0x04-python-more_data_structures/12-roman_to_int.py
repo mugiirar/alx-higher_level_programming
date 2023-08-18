c2ca65b0b6deb77f3f5dd64fa7c88877e4267653
mugiirar <munenevincentmugiira@gmail.com>
2023-08-17 17:29:58 -0700

    romans

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    roman_s = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    total = 0
+    prev_value = 0
+    
+    for numeral in reversed(roman_string):
+        value = roman_s[numeral]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+        
+    return total
