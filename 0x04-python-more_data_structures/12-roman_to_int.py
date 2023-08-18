2382d936dea0a9ecb64d9ebc20ce1ac1d2069797
mugiirar <munenevincentmugiira@gmail.com>
2023-08-17 17:52:26 -0700

    weighted avarage

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,11 @@ def roman_to_int(roman_string):
 
     roman_s = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
+    if not isinstance(roman_string, str):
+        return 0
+
+    if roman_string is None:
+        return 0
     total = 0
     prev_value = 0
     
