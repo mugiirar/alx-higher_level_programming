52dd3c41e9233890a8b142a81ca77878ff0db211
mugiirar <munenevincentmugiira@gmail.com>
2023-08-19 03:33:17 -0700

    ch

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+# 7-islower.py
+
 def islower(c):
-    return 97 <= ord(c) <= 122
+
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
