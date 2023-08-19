17a8d3f7a595782553ad968a38a433a95a10fc6f
mugiirar <munenevincentmugiira@gmail.com>
2023-08-19 01:04:28 -0700

    hex

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,34 +1,5 @@
 #!/usr/bin/python3
+# 4-print_hexa.py
 
-n = 0
-
-while n <= 98:
-    if n >= 0 and n <= 10:
-        k = str(n)
-
-    if n >= 10 and n <= 15:
-        lett = "abcdef"
-
-        k = lett[n - 10]
-
-    if n > 15:
-
-        p = str(int(n / 16))
-
-        m = str(n % 16)
-
-        k = p[0] + m
-        if len(k) > 2:
-            y = p[0]
-            d = int(k[1] + k[2])
-
-            if d >= 10 and d <= 15:
-                letters = "abdcef"
-                v = letters[d - 10]
-                k = y + v
-
-        l = str(n)
-
-    print(str(n) + " = 0x" + k)
-    n = int(n)
-    n += 1
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
