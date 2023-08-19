ced9aa67173ec41b92eb9d186e6365e3bb5f8c3f
mugiirar <munenevincentmugiira@gmail.com>
2023-08-19 01:15:19 -0700

    hex

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # 4-print_hexa.py
 
-for number in range(0, 99):
-    print("{} = {}".format(number, hex(number)))
+for number in range(99):
+    print("{:d} = 0x{:x}".format(number, number))
