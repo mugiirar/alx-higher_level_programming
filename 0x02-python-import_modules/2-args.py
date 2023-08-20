256a2d94a70d9e50dc9fa0e7f8085731accfc3eb
mugiirar <munenevincentmugiira@gmail.com>
2023-08-20 07:27:57 -0700

    exec

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import sys
+    length = len(sys.argv) - 1
 
-length = len(sys.argv) - 1
+    if length == 0:
+        print("0 arguments.")
 
-if length == 0:
-    print("0 arguments.")
+    if length == 1:
+        print("1 argument:")
+        print("1: " + sys.argv[1])
 
-if length == 1:
-    print("1 argument:")
-    print("1: " + sys.argv[1])
-
-if length > 2:
-    print ("{} arguments:".format(length))
-    for k in range(1, length + 1):
-        st = sys.argv[k]
-        print("{}: ".format(k) + st)
+    if length > 2:
+        print ("{} arguments:".format(length))
+        for k in range(1, length + 1):
+            st = sys.argv[k]
+            print("{}: ".format(k) + st)
