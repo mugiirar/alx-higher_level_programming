5adb44cf7223ac6a99c4197c9e50588763ebb8bb
mugiirar <munenevincentmugiira@gmail.com>
2023-08-28 03:48:22 -0700

    division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,8 +6,5 @@ def safe_print_division(a, b):
     except  (ZeroDivisionError,  FloatingPointError):
         result = None
     finally:
-        if result != None:
-            print("Inside result: {}".format(result))
-        else:
-            print("Inside result: None")
+        print("Inside result: {}".format(result))
         return result
