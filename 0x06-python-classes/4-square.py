78379e9f69a8db17b50e3f9df5a387779c9fcc4a
mugiirar <munenevincentmugiira@gmail.com>
2023-08-29 04:36:06 -0700

    try

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+"""definition of class"""
+
+
+class Square:
+    """body """
+    def __init__(self, size=0):
+        """Square"""
+        self.size = size
+
+    def size(self):
+        return (self.__size)
+
+
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area"""
+        return(self.__size * self.__size)
