50d2c7b40c7927f0b9e8c23b407cba0b30dfd3e8
mugiirar <munenevincentmugiira@gmail.com>
2023-09-11 09:21:43 -0700

    same class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""checks if f the object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Write a function that returns True if the object is an instance"""
+    if isinstance(obj, a_class):
+        return (True)
+    return (False)
