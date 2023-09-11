058590d2e442429a4a95fdfb5c989aca7a999672
mugiirar <munenevincentmugiira@gmail.com>
2023-09-11 10:32:50 -0700

    improved geo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class for geometry"""
+
+
+class BaseGeometry:
+    """defination of this class"""
+    def area(self):
+        """complain"""
+        raise Exception("area() is not implemented")
