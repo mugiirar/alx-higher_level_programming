6a6897121de605e4d2f982fd70748d4a8e18a17a
mugiirar <munenevincentmugiira@gmail.com>
2023-09-12 15:00:37 -0700

    filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""defination of a student"""
+
+
+class Student:
+    """representation of student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        return {k: getattr(self, k) for k in attrs if isinstance(k, str) and hasattr(self, k)}
