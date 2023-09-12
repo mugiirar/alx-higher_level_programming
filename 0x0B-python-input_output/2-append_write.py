0a013b8349c37a7b4de525701e50f25bfb859fd7
mugiirar <munenevincentmugiira@gmail.com>
2023-09-12 13:35:32 -0700

    appending

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    with open(filename, 'a', encoding='utf-8') as file:
+    """appemding to file"""
+    with open(filename, "a", encoding="utf-8") as file:
         added = file.write(text)
         return added
