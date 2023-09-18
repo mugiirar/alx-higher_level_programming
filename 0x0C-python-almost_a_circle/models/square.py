8760d39571ef83e86733a85e112d26d411eae7ab
mugiirar <munenevincentmugiira@gmail.com>
2023-09-18 12:56:50 -0700

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
+"""implementation of the square class
+"""
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
+    """defination of square body
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """initialisation of the class
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """string representation of the object
+        """
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.width)
     
     @property
     def size(self):
+        """give back the public attribute of width
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """setting the size value
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -23,6 +36,8 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """update the object of square
+        """
         if len(args) == 0:
             for key, value in kwargs.items():
                 setattr(self, key, value)
@@ -40,6 +55,8 @@ class Square(Rectangle):
             k += 1
 
     def to_dictionary(self):
+        """dectionary representation
+        """
         my_dict = {
                 "id": self.id,
                 "size": self.size,
