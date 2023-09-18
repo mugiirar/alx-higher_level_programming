28c915159eed5ee61c832d0e606fc19deb2864d3
mugiirar <munenevincentmugiira@gmail.com>
2023-09-18 10:24:56 -0700

    testing

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines unittest for models/rectangle.py.
+"""
+import io
+import sys
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+    unittest.main()
