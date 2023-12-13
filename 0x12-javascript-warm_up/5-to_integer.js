252e4c7a5c85a75ebac7bc9928e535aea46c74cb
mugiirar <munenevincentmugiira@gmail.com>
2023-12-13 09:51:47 -0800

    five

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const num = Number(process.argv[2]);
-const integ = parseInt(num);
-console.log(isNaN(num) ? "Not a number" : "My number: " + String(integ));
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
