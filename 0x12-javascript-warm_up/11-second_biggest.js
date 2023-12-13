7cd1d009468de5822d31953f40b78ab438d9e966
mugiirar <munenevincentmugiira@gmail.com>
2023-12-13 10:11:40 -0800

    11 up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-let limit = 0;
-if (args.length > 1) {
-	limit = args.sort()[args.length - 2];
+if (process.argv.length <= 3) {
+	console.log(0);
+} else {
+	const args = process.argv.map(Number)
+		.slice(2, process.argv.length)
+		.sort((a, b) => a - b);
+	console.log(args[args.length - 2]);
 }
-console.log(limit);
