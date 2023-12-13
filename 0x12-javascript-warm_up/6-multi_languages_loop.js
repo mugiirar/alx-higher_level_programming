2420e90316d14af7fef21198134f3d061d692b32
mugiirar <munenevincentmugiira@gmail.com>
2023-12-13 09:53:18 -0800

    six

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,16 +1,4 @@
 #!/usr/bin/node
-
-let num = [
-	"C is fun",
-	"Python is cool",
-	"JavaScript is amazing"
-]
-
-let limit = num.length;
-
-let count = 0;
-while (count < limit) {
-	console.log(num[count]);
-	count = count + 1;
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
-
