560953ffc497e6c38e247a351bbe16a70bf06ef1
mugiirar <munenevincentmugiira@gmail.com>
2023-12-15 16:00:02 -0800

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""
+Lists all states with a name
+"""
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
     # Check if the correct number of command-line arguments is provided
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(argv[0]))
-        exit(1)
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
+        sys.exit(1)
 
     # Use a context manager to ensure proper handling of resources
-    with MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8") as db:
+    with MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]) as db:
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-        rows = cursor.fetchall()
-        
-        for row in rows:
-            print (row)
+        cursor.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id`")
+        states_starting_with_n = cursor.fetchall()
+
+        # Use a more explicit loop for better readability
+        for state in states_starting_with_n:
+            print(state)
+
