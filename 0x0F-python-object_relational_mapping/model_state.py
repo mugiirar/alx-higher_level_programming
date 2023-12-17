fdbc7f8d0fe7c84948a991b36738487bb8e61a8f
mugiirar <munenevincentmugiira@gmail.com>
2023-12-17 08:01:04 -0800

    statee

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """
-Defines a city
+Defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table 'states'.
 """
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class City(Base):
-    """Represents a city in a MySQL database.
+class State(Base):
+    """
+    Represents a state for a MySQL database.
 
     Attributes:
-        id (int): The city's id.
-        name (str): The city's name.
-        state_id (int): The city's state id.
+        __tablename__ (str): The name of the MySQL table to store States.
+        id (sqlalchemy.Integer): The state's id.
+        state_name (sqlalchemy.String): The state's name.
     """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)  # Adjusted the length for flexibility
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, doc="The state's unique identifier.")
+    state_name = Column(String(128), nullable=False, doc="The name of the state.")
+
+    def __repr__(self):
+        """
+        Return a string representation of the State model.
+        """
+        return f"<State(id={self.id}, state_name={self.state_name})>"
+
