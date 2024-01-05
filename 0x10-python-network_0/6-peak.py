3f606a7ddb3fa6b9635dec40853d0549742de822
mugiirar <munenevincentmugiira@gmail.com>
2024-01-05 12:22:55 -0800

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# finding largest number
+def find_peak(list_of_integers):
+    num = len(list_of_integers)
+    
+    if (num == 0):
+        return None
+
+    else:
+
+        count = 0
+        ans = list_of_integers[0]
+
+        while (count < num):
+            track = list_of_integers[count]
+            for seg in list_of_integers:
+                if seg > track:
+                    ans = seg
+
+            count = count + 1
+        
+        return ans
