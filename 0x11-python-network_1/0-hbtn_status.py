10777c6a6c1877e8ac7b85b92e6933270915c6d1
mugiirar <munenevincentmugiira@gmail.com>
2024-01-06 06:15:14 -0800

    main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 """ Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
 
-req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
