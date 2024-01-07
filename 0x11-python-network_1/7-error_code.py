ebd4bffa7d04130967f807f74b896f0ac1982308
mugiirar <munenevincentmugiira@gmail.com>
2024-01-07 03:01:06 -0800

    r text

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,4 +8,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-    print(req.status_code)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
