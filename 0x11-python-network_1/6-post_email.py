789a47c1f3071017f7e1d1aa2ab3d49e948cf305
mugiirar <munenevincentmugiira@gmail.com>
2024-01-07 02:33:41 -0800

    email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email address
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+
+    data = {"email" : sys.argv[2] }
+
+    req = requests.post(url, data=data)
+
+
