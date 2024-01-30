afc80beeb8bc658d728319113d5271da47bc7524
mugiirar <munenevincentmugiira@gmail.com>
2024-01-30 07:08:32 -0800

    lorus

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,15 @@
 #!/usr/bin/node
+// contents of a webpage and stores it in a file
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const args = process.argv.slice(2);
+
+request.get(args[0], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(args[1], body, function (err, res) {
+      if (err) console.log('error', err);
+    });
+  }
+});
