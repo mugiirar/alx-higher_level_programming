c6b917e8fbaabb9a5552f0b436b8f758d83cdb83
mugiirar <munenevincentmugiira@gmail.com>
2024-01-30 09:53:58 -0800

    completer

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-
 const request = require('request');
 const apiUrl = process.argv[2];
 
@@ -10,24 +9,23 @@ request(apiUrl, function (error, response, responseBody) {
   } else {
     const tasksCompletedByUser = {};
     const tasks = JSON.parse(responseBody);
-    
+
     for (let i = 0; i < tasks.length; i++) {
       const currentTask = tasks[i];
-      
+
       if (!(currentTask.userId in tasksCompletedByUser)) {
         tasksCompletedByUser[currentTask.userId] = 0;
       }
-      
+
       if (currentTask.completed) {
         tasksCompletedByUser[currentTask.userId] += 1;
       }
-      
+
       if (tasksCompletedByUser[currentTask.userId] === 0) {
         delete tasksCompletedByUser[currentTask.userId];
       }
     }
-    
+
     console.log(tasksCompletedByUser);
   }
 });
-
