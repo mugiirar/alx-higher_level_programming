ac802ab1c85d3e72afd1192a9d1146dfe321a4ab
mugiirar <munenevincentmugiira@gmail.com>
2024-01-30 06:51:03 -0800

    write

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+
+const args = process.argv.slice(2);
+
+fs.writeFile(args[0], args[1], (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
